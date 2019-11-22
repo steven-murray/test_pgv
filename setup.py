4eab39343d90edbff4ba12507dff38fc19578a3c
Steven Murray <steven.murray@curtin.edu.au>
2019-11-22 13:34:05 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import io
+from os.path import dirname, join
+
+from setuptools import find_packages, setup
+
+import pygitversion
+pygitversion.write_git_info_file("test_pgv")
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
+    ) as fh:
+        return fh.read()
+
+setup(
+    name="test_pgv",
+    version="0.1.0",
+    license="MIT",
+    description="Test-repo for pygitversion",
+    author="Radio Astronomy Software Group",
+    author_email="steven.g.murray@asu.edu",
+    url="https://github.com/steven-murray/test_pgv",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.5",
+    install_requires=['pygitversion'],
+)
---
+++ test_pgv/__init__.py
@@ -0,0 +1,4 @@
+import pygitversion
+
+__version__ = '0.1.0'
+GIT_VERSION = pygitversion.construct_version_info(__name__)
