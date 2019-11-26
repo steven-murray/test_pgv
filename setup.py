75237e87af3fb92c8206c084874e562307419d41
Steven Murray <steven.murray@curtin.edu.au>
2019-11-26 16:11:48 -0700

    change to scm

--- setup.py
+++ setup.py
@@ -1,49 +1,21 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-from __future__ import absolute_import, print_function
-from setuptools.command.install import install
 
-import io
-from os.path import dirname, join
 from setuptools import find_packages, setup
 
-import pygitversion
-pygitversion.write_git_info_file(__file__, 'test_pgv')
+def myversion():
+    from setuptools_scm.version import guess_next_dev_version
 
+    def branch_scheme(version):
+        return guess_next_dev_version(version) if version.exact else guess_next_dev_version(version)+"b{}".format(version.branch)
 
-def read(*names, **kwargs):
-    with io.open(
-        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
-    ) as fh:
-        return fh.read()
+    return {'local_scheme': branch_scheme, "version_scheme": branch_scheme}
 
 setup(
     name="test_pgv",
-    version="0.1.0",
-    license="MIT",
-    description="Test-repo for pygitversion",
-    author="Radio Astronomy Software Group",
-    author_email="steven.g.murray@asu.edu",
-    url="https://github.com/steven-murray/test_pgv",
     packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Utilities",
-    ],
     python_requires=">=3.5",
     install_requires=['pygitversion'],
+    use_scm_version=myversion,
+    setup_requires=['setuptools_scm']
 )
